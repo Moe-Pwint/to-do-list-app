4968c3c6948b4a2731c9b70bf8d1c1a7442894e8
Moe Pwint <moezatmoepwint@gmail.com>
2025-05-29 21:35:45 +0700

    Create new task details UI in add-task.js.

--- src/add-task.js
+++ src/add-task.js
@@ -2,9 +2,12 @@ import "./styles.css";
 import createMainElements, {createEle} from "./main-ui";
 
 import landscapeDots from "./svg/landscapeDots.svg";
+import plusPurple from "./svg/plusPurple.svg";
+import folderSearch from "./svg/folderSearch.svg";
+import newFolder from "./svg/newFolder.svg";
 
 createMainElements();
-//const rightContainer = document.querySelector('#rightContainer');
+
 const addTaskBtn = document.querySelector('.addTask');
 
 addTaskBtn.addEventListener('click',() => {
@@ -17,7 +20,7 @@ function changeAddTaskTab () {
     addTaskBtn.classList.add('active-yellow');
     const landscapeDotsIcon = createEle('img');
     landscapeDotsIcon.src = landscapeDots;
-    landscapeDotsIcon.setAttribute('class','icons');
+    landscapeDotsIcon.setAttribute('class','left-side-icons');
     addTaskBtn.appendChild(landscapeDotsIcon);
 
     const addingTaskText = createEle('p');
@@ -27,11 +30,130 @@ function changeAddTaskTab () {
 }
 
 function createTaskDetailsBox() {
+    const addNewTaskPage = createEle('div');
+    addNewTaskPage.id = 'addNewTaskPage';
+    rightContainer.appendChild(addNewTaskPage);
+
     const newTaskDetailsBox = createEle('div');
     newTaskDetailsBox.id = "newTaskDetailsBox";
-    rightContainer.appendChild(newTaskDetailsBox);
+    addNewTaskPage.appendChild(newTaskDetailsBox);
 
     const taskDetailsTitle = createEle('h1');
     taskDetailsTitle.textContent = "New Task Details:";
+    taskDetailsTitle.id = 'taskDetailsTitle';
     newTaskDetailsBox.appendChild(taskDetailsTitle);
+
+        //task Name tab
+    const taskNameContainer = createEle('div');
+    taskNameContainer.setAttribute('class', 'taskDetailsContainers');
+    newTaskDetailsBox.appendChild(taskNameContainer);
+
+    const taskNameLabel = createTaskDetailsLabel('taskName', '*Task Name:');
+    taskNameContainer.appendChild(taskNameLabel);
+
+    const taskNameInput = createTaskDetailsInput('text', 'taskName', 'Add task name');
+    taskNameContainer.appendChild(taskNameInput);
+
+    const taskNameAddBtn = createAddButton();
+    taskNameContainer.appendChild(taskNameAddBtn);
+
+        //choose project folder tab
+    const setFolderContainer = createEle('div');
+    setFolderContainer.setAttribute('class', 'taskDetailsContainers');
+    newTaskDetailsBox.appendChild(setFolderContainer);
+
+    const setFolderLabel = createTaskDetailsLabel('setFolder', '*Project folder:');
+    setFolderContainer.appendChild(setFolderLabel);
+
+    const folderTabsContainer = createEle('div');
+    folderTabsContainer.setAttribute('class', 'folderTabs');
+    setFolderContainer.appendChild(folderTabsContainer);
+
+    const chooseFolderAddBtn = createButton(folderSearch, 'choose project folder');
+    chooseFolderAddBtn.setAttribute('class', 'setProjectFolderBtn');
+    folderTabsContainer.appendChild(chooseFolderAddBtn);
+
+    const newFolderAddBtn = createButton(newFolder, 'create new project');
+    newFolderAddBtn.setAttribute('class', 'setProjectFolderBtn');
+    folderTabsContainer.appendChild(newFolderAddBtn);
+
+        //Description tab
+    const taskDescriptionContainer = createEle('div');
+    taskDescriptionContainer.setAttribute('class', 'taskDetailsContainers');
+    newTaskDetailsBox.appendChild(taskDescriptionContainer);
+
+    const taskDescriptionLabel = createTaskDetailsLabel('taskDescription', 'Description:');
+    taskDescriptionContainer.appendChild(taskDescriptionLabel);
+
+    const taskDescriptionInput = createTaskDetailsInput('text', 'taskDescription');
+    taskDescriptionContainer.appendChild(taskDescriptionInput);
+
+    const taskDescriptionAddBtn = createAddButton();
+    taskDescriptionContainer.appendChild(taskDescriptionAddBtn);
+
+        //Notes tab
+    const taskNotesContainer = createEle('div');
+    taskNotesContainer.setAttribute('class', 'taskDetailsContainers');
+    newTaskDetailsBox.appendChild(taskNotesContainer);
+
+    const taskNotesLabel = createTaskDetailsLabel('taskNotes', 'Notes:');
+    taskNotesContainer.appendChild(taskNotesLabel);
+
+    const taskNotesInput = createTaskDetailsInput('text', 'taskNotes');
+    taskNotesContainer.appendChild(taskNotesInput);
+
+    const taskNotesAddBtn = createAddButton();
+    taskNotesContainer.appendChild(taskNotesAddBtn);
+
+        //Main Action Buttons tab
+    const mainActionBtnsContainer = createEle('div');
+    mainActionBtnsContainer.setAttribute('class', 'mainActionBtnsContainer');
+    newTaskDetailsBox.appendChild(mainActionBtnsContainer);
+
+    const addTaskActionBtn = createEle('button');
+    addTaskActionBtn.setAttribute('class', 'mainActionBtns addActionBtn');
+    addTaskActionBtn.textContent = 'Add new Task';
+    mainActionBtnsContainer.appendChild(addTaskActionBtn);
+
+    const newTaskCancelBtn = createEle('button');
+    newTaskCancelBtn.setAttribute('class', 'mainActionBtns cancelActionBtn');
+    newTaskCancelBtn.textContent = 'Cancel Task';
+    mainActionBtnsContainer.appendChild(newTaskCancelBtn);
+}
+
+function createTaskDetailsLabel (forValue, text) {
+    const label = createEle('label');
+    label.setAttribute('for', forValue);
+    label.textContent = text;
+    return label;
+
+}
+
+function createTaskDetailsInput(typeValue, idValue, defaultText) {
+    const input = createEle('input');
+    input.setAttribute('type', typeValue);
+    input.id = idValue;
+    input.setAttribute('name', idValue);
+    defaultText && input.setAttribute('placeholder', defaultText);
+    return input;
+}
+
+function createButton(iconSrc, textValue) {
+    const button = createEle('button');
+    
+    const icon = createEle('img');
+    icon.src = iconSrc;
+    button.appendChild(icon);
+
+    const text = createEle('p');
+    text.textContent = textValue;
+    button.appendChild(text);
+
+    return button;
+} 
+
+function createAddButton () {
+    const addBtn = createButton(plusPurple, 'Add');
+    addBtn.setAttribute('class', 'plusButton');
+    return addBtn;
 }
--- src/main-ui.js
+++ src/main-ui.js
@@ -18,138 +18,138 @@ function createEle (ele) {
 function createMainElements () {
 
 
-//main containers
-const appContainer = createEle('div');
-appContainer.id = 'appContainer';
-document.body.appendChild(appContainer);
+    //main containers
+    const appContainer = createEle('div');
+    appContainer.id = 'appContainer';
+    document.body.appendChild(appContainer);
 
-const leftContainer = createEle('div');
-leftContainer.id = 'leftContainer';
-appContainer.appendChild(leftContainer);
+    const leftContainer = createEle('div');
+    leftContainer.id = 'leftContainer';
+    appContainer.appendChild(leftContainer);
 
-const rightContainer = createEle('div');
-rightContainer.id = 'rightContainer';
-appContainer.appendChild(rightContainer);
-
-
-
-//logo section
-const logoContainer = createEle('div');
-logoContainer.id = 'logoContainer';
-leftContainer.appendChild(logoContainer);
-
-const logo = createEle("img");
-logo.src = odinImage;
-logo.id= 'logo';
-logoContainer.appendChild(logo);
-
-const logoText = createEle('p');
-logoText.textContent = 'DoMe';
-logoText.id = 'logoText';
-logoContainer.appendChild(logoText);
-
-
-// top left tabs
-const topLeftContainer = createEle('div');
-topLeftContainer.id = 'topLeftContainer';
-leftContainer.appendChild(topLeftContainer);
-
-    //Add new task tab
-const newTaskContainer = createEle('button');
-//addTask class will do adding task logic.
-newTaskContainer.setAttribute('class','addTask top-left-tabs');
-topLeftContainer.appendChild(newTaskContainer);
-
-const plusIcon = createEle('img');
-plusIcon.src = plus;
-plusIcon.setAttribute('class','icons');
-newTaskContainer.appendChild(plusIcon);
-
-const newTaskText = createEle('p');
-newTaskText.textContent = 'Add a new task';
-newTaskContainer.appendChild(newTaskText);
-
-    //Search tab
-const searchContainer = createEle('button');
-//searchLogic class will do search logic.
-searchContainer.setAttribute('class','searchLogic top-left-tabs');
-topLeftContainer.appendChild(searchContainer);
-
-const searchIcon = createEle('img');
-searchIcon.src = search;
-searchIcon.setAttribute('class','icons');
-searchContainer.appendChild(searchIcon);
-
-const searchText = createEle('p');
-searchText.textContent = 'Search task or project';
-searchContainer.appendChild(searchText);
-
-    //Today tab
-const todayContainer = createEle('button');
-//todayLogic class will do today logic.
-todayContainer.setAttribute('class','todayLogic top-left-tabs');
-topLeftContainer.appendChild(todayContainer);
-
-const todayIcon = createEle('img');
-todayIcon.src = today;
-todayIcon.setAttribute('class','icons');
-todayContainer.appendChild(todayIcon);
-
-const todayText = createEle('p');
-todayText.textContent = 'Today';
-todayContainer.appendChild(todayText);
-
-
-//Bottom section
-const projectsTitleContainer = createEle('div');
-projectsTitleContainer.setAttribute('id', 'projectsTitleContainer');
-leftContainer.appendChild(projectsTitleContainer);
-
-const allProjectsTitle = createEle('h2');
-allProjectsTitle.textContent = 'All Projects';
-projectsTitleContainer.appendChild(allProjectsTitle);
-
-    //Add Project icon
-const addProjectIcon = createEle('button');
-addProjectIcon.id = 'addProjectIcon';
-const projectIcon = createEle('img');
-projectIcon.src = addProject;
-projectIcon.setAttribute('class','icons');
-projectIcon.id = 'projectIcon';
-addProjectIcon.appendChild(projectIcon);
-projectsTitleContainer.appendChild(addProjectIcon); 
+    const rightContainer = createEle('div');
+    rightContainer.id = 'rightContainer';
+    appContainer.appendChild(rightContainer);
 
 
 
-//PROJECTS DISPLAY
+    //logo section
+    const logoContainer = createEle('div');
+    logoContainer.id = 'logoContainer';
+    leftContainer.appendChild(logoContainer);
 
-const projectsContainer = createEle('div');
-projectsContainer.id = 'projectsContainer';
-leftContainer.appendChild(projectsContainer);
+    const logo = createEle("img");
+    logo.src = odinImage;
+    logo.id= 'logo';
+    logoContainer.appendChild(logo);
 
-//Deleted tasks
+    const logoText = createEle('p');
+    logoText.textContent = 'DoMe';
+    logoText.id = 'logoText';
+    logoContainer.appendChild(logoText);
 
-const delContainer = createEle('div');
-leftContainer.appendChild(delContainer);
 
-const delTab = createEle('button');
-//this is common class for all dynamically created projects.
-delTab.setAttribute('class', 'projects-tabs purple-bg');
-delTab.id = 'del-tab';
-delContainer.appendChild(delTab);
+    // top left tabs
+    const topLeftContainer = createEle('div');
+    topLeftContainer.id = 'topLeftContainer';
+    leftContainer.appendChild(topLeftContainer);
 
-const delIcon = createEle('img');
-delIcon.src = del;
-delIcon.setAttribute('class', 'icons');
-delTab.appendChild(delIcon);
+        //Add new task tab
+    const newTaskContainer = createEle('button');
+    //addTask class will do adding task logic.
+    newTaskContainer.setAttribute('class','addTask top-left-tabs');
+    topLeftContainer.appendChild(newTaskContainer);
 
-const delTabText = createEle('p');
-delTabText.textContent = 'Deleted Tasks';
-delTab.appendChild(delTabText);
+    const plusIcon = createEle('img');
+    plusIcon.src = plus;
+    plusIcon.setAttribute('class','left-side-icons');
+    newTaskContainer.appendChild(plusIcon);
 
-const portraitDotsIcon = createEle('img');
-portraitDotsIcon.src = portraitDots;
-portraitDotsIcon.setAttribute('class', 'icons push-right');
-delTab.appendChild(portraitDotsIcon);
+    const newTaskText = createEle('p');
+    newTaskText.textContent = 'Add a new task';
+    newTaskContainer.appendChild(newTaskText);
+
+        //Search tab
+    const searchContainer = createEle('button');
+    //searchLogic class will do search logic.
+    searchContainer.setAttribute('class','searchLogic top-left-tabs');
+    topLeftContainer.appendChild(searchContainer);
+
+    const searchIcon = createEle('img');
+    searchIcon.src = search;
+    searchIcon.setAttribute('class','left-side-icons');
+    searchContainer.appendChild(searchIcon);
+
+    const searchText = createEle('p');
+    searchText.textContent = 'Search task or project';
+    searchContainer.appendChild(searchText);
+
+        //Today tab
+    const todayContainer = createEle('button');
+    //todayLogic class will do today logic.
+    todayContainer.setAttribute('class','todayLogic top-left-tabs');
+    topLeftContainer.appendChild(todayContainer);
+
+    const todayIcon = createEle('img');
+    todayIcon.src = today;
+    todayIcon.setAttribute('class','left-side-icons');
+    todayContainer.appendChild(todayIcon);
+
+    const todayText = createEle('p');
+    todayText.textContent = 'Today';
+    todayContainer.appendChild(todayText);
+
+
+    //Bottom section
+    const projectsTitleContainer = createEle('div');
+    projectsTitleContainer.setAttribute('id', 'projectsTitleContainer');
+    leftContainer.appendChild(projectsTitleContainer);
+
+    const allProjectsTitle = createEle('h2');
+    allProjectsTitle.textContent = 'All Projects';
+    projectsTitleContainer.appendChild(allProjectsTitle);
+
+        //Add Project icon
+    const addProjectIcon = createEle('button');
+    addProjectIcon.id = 'addProjectIcon';
+    const projectIcon = createEle('img');
+    projectIcon.src = addProject;
+    projectIcon.setAttribute('class','left-side-icons');
+    projectIcon.id = 'projectIcon';
+    addProjectIcon.appendChild(projectIcon);
+    projectsTitleContainer.appendChild(addProjectIcon); 
+
+
+
+    //PROJECTS DISPLAY
+
+    const projectsContainer = createEle('div');
+    projectsContainer.id = 'projectsContainer';
+    leftContainer.appendChild(projectsContainer);
+
+    //Deleted tasks
+
+    const delContainer = createEle('div');
+    leftContainer.appendChild(delContainer);
+
+    const delTab = createEle('button');
+    //this is common class for all dynamically created projects.
+    delTab.setAttribute('class', 'projects-tabs');
+    delTab.id = 'del-tab';
+    delContainer.appendChild(delTab);
+
+    const delIcon = createEle('img');
+    delIcon.src = del;
+    delIcon.setAttribute('class', 'left-side-icons');
+    delTab.appendChild(delIcon);
+
+    const delTabText = createEle('p');
+    delTabText.textContent = 'Deleted Tasks';
+    delTab.appendChild(delTabText);
+
+    const portraitDotsIcon = createEle('img');
+    portraitDotsIcon.src = portraitDots;
+    portraitDotsIcon.setAttribute('class', 'left-side-icons push-right');
+    delTab.appendChild(portraitDotsIcon);
 }
 
